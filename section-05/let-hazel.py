df12647561423fa8d9ab731b67ce528f4728e77a
let-Hazel <146333729+let-Hazel@users.noreply.github.com>
2025-10-21 11:56:33 +0200

    program sum of positive numbers

--- section-05/let-hazel.py
+++ section-05/let-hazel.py
@@ -1,1 +1,9 @@
+def sum_of_positive_squares(n):
+  if n <= 0:
+    raise ValueError
+  for i in range(n):
+    if i > 0:
+      f = i * (i + 1) * (2 * i + 1)//6
+  return f
 
+sum_of_positive_squares(3)
