cd52f6aa0f1b9227e027cfa9520a10e6ccc8affb
Yaseerah <151749768+Yaseerahk@users.noreply.github.com>
2025-10-22 14:02:52 +0200

    Add simple addition program in yaseerah.py

---
+++ section-04/yaseerah.py
@@ -0,0 +1,13 @@
+
+# create an integer variable for x
+# create an integer variable for y
+# ask the user "X: " and put answer in x
+# ask the user "Y: " and put answer in y
+# put x + y in sum
+# tell user "Answer is " sum
+
+
+x = int(input("X: "))
+y = int(input("Y: "))
+sum = x + y
+print("Answer is", sum)
