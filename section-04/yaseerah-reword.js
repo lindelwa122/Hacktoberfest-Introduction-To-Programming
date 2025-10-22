be0d3d0f1c98539d0bf63afb1fae5a6444b3a070
Yaseerah <151749768+Yaseerahk@users.noreply.github.com>
2025-10-22 14:19:06 +0200

    Delete section-04/yaseerah-reword.js

--- section-04/yaseerah-reword.js
+++
@@ -1,3 +0,0 @@
-Problem:
-I need to write a program that asks the user for a positive number n.
-Then, the program must calculate the total sum of the squares of all numbers smaller than n, if n = 5, it should calculate (1² + 2² + 3² + 4²) and display the result.
